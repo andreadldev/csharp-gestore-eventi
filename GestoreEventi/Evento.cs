67f7c048dee1fa5c6dd012ef401916d9069b824e
andreadldev <andrea.delisi2@gmail.com>
2023-03-27 11:45:19 +0200

    Classe evento, costruttore e controlli setter

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        private string titolo;
+        private DateTime data;
+        private int capienza;
+        private int postiPrenotati;
+
+        public string Titolo
+        {
+            get => titolo;
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                {
+                    titolo = value;
+                }
+                else throw new Exception("Il titolo non può essere una stringa vuota");
+            }
+        }
+
+        public DateTime Data
+        {
+            get => data;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    data = value;
+                }
+                else throw new Exception("La data non può essere una data passata");
+            }
+        }
+
+        public int Capienza {
+            get => capienza;
+            private set
+            {
+                if (value > 0)
+                {
+                    capienza = value;
+                }
+                else throw new Exception("La capienza massima non può essere zero o un numero negativo");
+            }
+        }
+
+        public int PostiPrenotati
+        {
+            get => postiPrenotati;
+            private set { }
+        }
+
+        public Evento(string titolo, DateTime data, int capienza)
+        {
+            Titolo = titolo;
+            Data = data;
+            Capienza = capienza;
+            postiPrenotati = 0;
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GestoreEventi;
+
+var test = new Evento("eventoprova", new DateTime(2023, 03, 28), 50);
+Console.WriteLine(test.Titolo);
+Console.WriteLine(test.Data);
+Console.WriteLine(test.Capienza);
+Console.WriteLine(test.PostiPrenotati);
